6cee7981bee71f45d72dbc4016e03cc2c288e8fc
xtay2 <x-tay2@o2mail.de>
2022-04-30 14:40:59 +0200

    Installer Update

--- pseudocode-win-installer/pseudocode.cmd
+++ pseudocode-win-installer/pseudocode.cmd
@@ -6,17 +6,31 @@ if "%1" == "format" goto execute
 
 if "%1" == "uninstall" goto uninstall
 
+if "%1" == "update" goto update
+if "%1" == "enable-autoupdates" goto enAutoupdates
+if "%1" == "disable-autoupdates" goto disAutoupdates
+
 :invalid
 echo Invalid command. Execute "pseudocode help" for help.
 exit
 
 :help
 echo:
-echo --------HELP--------
-echo - To format and run a project, navigate to the project-dir and execute "pseudocode run".
-echo - To delete pseudocode and all your libraries, execute "pseudocode uninstall".
-echo --------------------
+echo ----------------------------------------------------------------------------------------------------------
+echo Execution
+echo   - To format and run a project, navigate to the project-dir and execute "pseudocode run".
+echo   - To just format a project, navigate to the project-dir and execute "pseudocode format".
 echo:
+echo Updating (Requires admin-privileges) -------------------------------------------------------------------
+echo   - To update pseudocode to the newest version, execute "pseudocode update".
+echo   - To enable autoupdates, execute "pseudocode enable-autoupdates". (This performs an immediate update) 
+echo   - To disable autoupdates, execute "pseudocode disable-autoupdates".
+echo:
+echo Uninstalling -------------------------------------------------------------------------------------------
+echo   - To delete pseudocode and all your libraries, execute "pseudocode uninstall".
+echo ----------------------------------------------------------------------------------------------------------
+echo:
+pause
 exit
 
 :execute
@@ -43,11 +57,23 @@ exit
 schtasks /delete /tn "Pseudocode Update" /f
 rmdir "%ProgramFiles%/Pseudocode" /Q /S 
 (goto) 2>nul & del "%~f0"
-echo Successfully uninstalled pseudocode.
+echo Successfully uninstalled pseudocode!
 pause
 exit
 
 :abortUninstall
-echo Aborted the uninstallation.
+echo Aborted the uninstallation!
+pause
+exit
+
+:enAutoupdates
+schtasks /create /sc onstart /tn "Pseudocode Update" /tr "%ProgramFiles%\Pseudocode"
+:update
+"%ProgramFiles%\Pseudocode\PseudocodeUpdate.bat"
+pause
+exit
+
+:disAutoupdates
+schtasks /delete /tn "Pseudocode Update" /f
 pause
 exit
