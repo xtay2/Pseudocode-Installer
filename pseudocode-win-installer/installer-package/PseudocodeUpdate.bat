ba77460a097ef9c453e2c4657626bab1ef9d3e78
xtay2 <x-tay2@o2mail.de>
2022-05-18 21:00:29 +0200

    Win-exe-installer

--- pseudocode-win-installer/installer-package/PseudocodeUpdate.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Program Files\Pseudocode
-curl "https://github.com/xtay2/Pseudocode-Installer/raw/main/pseudocode-win-installer/installer-package/Interpreter.jar" --output Interpreter.jar -O -L
--- pseudocode-win-installer/pseudocode.cmd
+++ pseudocode-win-installer/pseudocode.cmd
@@ -1,18 +1,9 @@
 @echo off
-if "%1" == "help" goto help
+
+FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Pseudocode Language\Pseudocode Interpreter" /v "Path"') do set pcPath=%%b\Pseudocode
 
 if "%1" == "run" goto execute
 if "%1" == "format" goto execute
-
-if "%1" == "uninstall" goto uninstall
-
-if "%1" == "update" goto update
-if "%1" == "enable-autoupdates" goto enAutoupdates
-if "%1" == "disable-autoupdates" goto disAutoupdates
-
-:invalid
-echo Invalid command. Execute "pseudocode help" for help.
-exit
 
 :help
 echo:
@@ -20,60 +11,11 @@ echo ---------------------------------------------------------------------------
 echo Execution
 echo   - To format and run a project, navigate to the project-dir and execute "pseudocode run".
 echo   - To just format a project, navigate to the project-dir and execute "pseudocode format".
-echo:
-echo Updating (Requires admin-privileges) -------------------------------------------------------------------
-echo   - To update pseudocode to the newest version, execute "pseudocode update".
-echo   - To enable autoupdates, execute "pseudocode enable-autoupdates". (This performs an immediate update) 
-echo   - To disable autoupdates, execute "pseudocode disable-autoupdates".
-echo:
-echo Uninstalling -------------------------------------------------------------------------------------------
-echo   - To delete pseudocode and all your libraries, execute "pseudocode uninstall".
 echo ----------------------------------------------------------------------------------------------------------
 echo:
 pause
 exit
 
 :execute
-java -jar --enable-preview "%ProgramFiles%/Pseudocode/interpreter.jar" "%ProgramFiles%/Pseudocode/lib" "%CD%" %1 %2 %3 %4 %5
-exit
-
-:uninstall
-echo Administrative permissions required. Detecting permissions...
-net session >nul 2>&1
-if %errorLevel% == 0 (
-	echo Success: Administrative permissions confirmed.
-) else (
-	echo Failure: Current permissions inadequate.
-	pause >nul
-    exit
-)
-echo Please confirm, that you want to uninstall pseudocode, including all your libraries.
-choice /c yn
-if %errorlevel%==1 goto confirmUninstall
-if %errorlevel%==2 goto abortUninstall
-exit
-
-:confirmUninstall
-schtasks /delete /tn "Pseudocode Update" /f
-rmdir "%ProgramFiles%/Pseudocode" /Q /S 
-(goto) 2>nul & del "%~f0"
-echo Successfully uninstalled pseudocode!
-pause
-exit
-
-:abortUninstall
-echo Aborted the uninstallation!
-pause
-exit
-
-:enAutoupdates
-schtasks /create /sc onstart /tn "Pseudocode Update" /tr "%ProgramFiles%\Pseudocode"
-:update
-"%ProgramFiles%\Pseudocode\PseudocodeUpdate.bat"
-pause
-exit
-
-:disAutoupdates
-schtasks /delete /tn "Pseudocode Update" /f
-pause
+java -jar --enable-preview "%pcPath%/Interpreter.jar" "%pcPath%/lib" "%CD%" %1 %2 %3 %4 %5
 exit
